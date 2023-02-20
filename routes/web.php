80aecfb3278a279c3613f7972da5617ec5a289f3
Rubén Catalán <rucaju5@gmail.com>
2023-02-20 19:16:34 +0100

    prueba 1001

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,9 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/images/{nombre}', function ($nombre) {
+    $pathToFile = public_path().'/images/'.$nombre;
+    return response()->file($pathToFile);
+});
+
 require __DIR__.'/auth.php';
